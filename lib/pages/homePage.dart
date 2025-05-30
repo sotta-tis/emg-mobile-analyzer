e0ee3f972f5243b7b4dc5c753b54e6fede7f6e9f
sotta-tis <werfer.sou@gmail.com>
2025-05-30 14:14:01 +0900

    :hammer: widgetsディレクトリの作成

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:emg_mobile_analyzer/bleWidget.dart';
+import 'package:emg_mobile_analyzer/widgets/bleWidget.dart';
 
 class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
